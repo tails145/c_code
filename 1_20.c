639822baab844e26b817dbca9bf4977dfe17060a
tails145 <2298382666@qq.com>
2021-01-31 20:40:06 +0800

    1_20

---
+++ 1_20.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+<<<<<<< HEAD
+#define MAX 10;
+int main()
+{
+	int arr[MAX] = { 0 };
+	printf("%d\n", MAX);
+	return 0;
+	
+}
+=======
+//int main()
+//{
+//	int age = 18;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else
+//		printf("成年\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int age = 80;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if (age >= 18 && age < 28)
+//	printf("青年\n");
+//	else
+//		printf("老年\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 1;
+//	while (num <= 100)
+//	{
+//		if (1 == num % 2 )
+//		{
+//			printf("%d\n", num);
+//			num++;
+//		}
+//		else
+//			num++;
+//
+//		
+//	}
+//
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> test_1_20
